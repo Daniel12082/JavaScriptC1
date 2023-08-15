fa54a5d765ff569ca5ad66d97459e09fa9bd3643
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-14 20:14:21 -0500

    inicio java

---
+++ function.js
@@ -0,0 +1,40 @@
+//funcion que no retorna ni recibe parametros
+//function mensaje(){
+//    console.log("Esto es un mensaje")
+//}
+//mensaje()
+//console.log(mensaje())
+
+//funcion que reorna pero no recibe parametros
+//function retorno(){
+//    return "Hola world"
+//}
+//retorno()
+//console.log(typeof retorno())
+
+//funcion que no retorna pero recibe parametros
+//function areaRectangulo (b,h)
+//{
+//    resultado=b*h
+//    console.log(resultado)
+//}
+//areaRectangulo(5,6)
+
+//funcion que retorna y recibe parametros
+//function concatenacion (a,b){
+//    return a+b
+//}
+//console.log(concatacion("campus", "lands"))
+
+function calcular(x){
+    actual=2023
+    nacimiento=actual-x
+    console.log("El año en el que naciste fue:"+final)
+}
+calcular(10)
+//calcular(23)
+function calcular(x){
+    val=32+(9*x/5)
+    console.log("La conversion de celcius a fahrenheit es:"+val )
+}
+calcular(30)
