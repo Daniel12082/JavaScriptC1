5140b2956eb5979b98c584e378338bd604363122
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-16 18:31:24 -0500

    reto1

--- Retos/password.js
+++ Retos/password.js
@@ -1,36 +1,47 @@
-
-// Código adaptado por [Tu Nombre] basado en el ejemplo de Daniel Díaz
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
-input: process.stdin,
-output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
+const longitud_minima = 1;
+const longitud_maxima = 25000;
 
-// Obtener el primer input
-rl.question('Ingresa el primer número: ', (input1) => {
-// Obtener el segundo input
-rl.question('Ingresa el segundo número: ', (input2) => {
-    // Convertir los inputs a números
-    const numero1 = parseFloat(input1);
-    const numero2 = parseFloat(input2);
+let nuevaContraseña = '';
 
-    // Verificar si los inputs son números válidos
-    if (isNaN(numero1) || isNaN(numero2)) {
-    console.log('Ingresa valores numéricos válidos.');
-    } else {
-    // Realizar operaciones o aplicar condiciones
-    if (numero1 > numero2) {
-        console.log(`${numero1} es mayor que ${numero2}.`);
-    } else if (numero1 < numero2) {
-        console.log(`${numero1} es menor que ${numero2}.`);
-    } else {
-        console.log('Los números son iguales.');
-    }
-    }
+// Función para verificar si una cadena es minúscula
+const isLowerCase = (str) => /^[a-z]+$/.test(str);
 
-    // Cerrar la interfaz
+// Función para cerrar la interfaz y mostrar la nueva contraseña
+const finalizar = () => {
     rl.close();
-});
-});
+    console.log(`Nueva contraseña generada: ${nuevaContraseña}`);
+};
+
+// Función para manejar las preguntas de las contraseñas
+const preguntarContraseñas = () => {
+    rl.question("Ingrese la primera contraseña: ", (password1) => {
+        rl.question("Ingrese la segunda contraseña: ", (password2) => {
+            if (password1.length !== password2.length) {
+                console.clear()
+                console.log("Las contraseñas no tienen la misma longitud");
+                preguntarContraseñas(); // Volver a preguntar
+            } else if ((password1.length < longitud_minima || password1.length > longitud_maxima) || (password2.length < longitud_minima || password2.length > longitud_maxima)) {
+                console.clear()
+                console.log("La contraseña debe tener una longitud mayor a 0 y menor a 25000");
+                preguntarContraseñas(); // Volver a preguntar
+            } else if (!isLowerCase(password1) || !isLowerCase(password2)) {
+                console.clear()
+                console.log('Las contraseñas deben contener solo letras minúsculas.');
+                preguntarContraseñas(); // Volver a preguntar
+            } else {
+                for (let i = 0; i < password1.length; i++) {
+                    nuevaContraseña += password1[i] + password2[i];
+                }
+                finalizar();
+            }
+        });
+    });
+};
+console.clear()
+preguntarContraseñas();
