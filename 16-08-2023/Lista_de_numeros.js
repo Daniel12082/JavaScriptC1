cc934601d44ba8be8847ac68d9f0a193e6f25361
“Daniel12082” <137439716+Daniel12082@users.noreply.github.com>
2023-08-16 13:03:08 -0500

    lista

---
+++ 16-08-2023/Lista_de_numeros.js
@@ -0,0 +1,13 @@
+/*##############################################
+#                 Ejercicio 6                  #
+##############################################*/
+
+//Desarrolado por: Daniel Felipe Diaz Sanchez
+
+let stringResult = " ";
+
+//Mientras el numero inicial 
+for (let a = 1; a <= 9; a++) {
+    stringResult += a + " ";
+    console.log(stringResult);
+}
