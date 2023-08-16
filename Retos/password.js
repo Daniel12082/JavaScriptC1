3f6831f7f569b1c035fda7e278cc2a5a6a3c0ff1
“Daniel12082” <137439716+Daniel12082@users.noreply.github.com>
2023-08-16 13:18:27 -0500

    a

--- Retos/password.js
+++ Retos/password.js
@@ -1,11 +1,36 @@
+
+// Código adaptado por [Tu Nombre] basado en el ejemplo de Daniel Díaz
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+input: process.stdin,
+output: process.stdout
 });
 
-rl.question('Ingresa algo: ', (respuesta) => {
-    console.log(`Has ingresado: ${respuesta}`);
+// Obtener el primer input
+rl.question('Ingresa el primer número: ', (input1) => {
+// Obtener el segundo input
+rl.question('Ingresa el segundo número: ', (input2) => {
+    // Convertir los inputs a números
+    const numero1 = parseFloat(input1);
+    const numero2 = parseFloat(input2);
+
+    // Verificar si los inputs son números válidos
+    if (isNaN(numero1) || isNaN(numero2)) {
+    console.log('Ingresa valores numéricos válidos.');
+    } else {
+    // Realizar operaciones o aplicar condiciones
+    if (numero1 > numero2) {
+        console.log(`${numero1} es mayor que ${numero2}.`);
+    } else if (numero1 < numero2) {
+        console.log(`${numero1} es menor que ${numero2}.`);
+    } else {
+        console.log('Los números son iguales.');
+    }
+    }
+
+    // Cerrar la interfaz
     rl.close();
+});
 });
