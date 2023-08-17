715f05e08814858cd42aa62f8b3b03f3086df0bb
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-08-17 07:04:27 -0500

    Update password.js

--- Retos/password.js
+++ Retos/password.js
@@ -7,6 +7,15 @@ const rl = readline.createInterface({
 const longitud_minima = 1;
 const longitud_maxima = 25000;
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const longitud_minima = 1;
+const longitud_maxima = 25000;
+
 let nuevaContraseña = '';
 
 // Función para verificar si una cadena es minúscula
@@ -22,11 +31,7 @@ const finalizar = () => {
 const preguntarContraseñas = () => {
     rl.question("Ingrese la primera contraseña: ", (password1) => {
         rl.question("Ingrese la segunda contraseña: ", (password2) => {
-            if (password1.length !== password2.length) {
-                console.clear()
-                console.log("Las contraseñas no tienen la misma longitud");
-                preguntarContraseñas(); // Volver a preguntar
-            } else if ((password1.length < longitud_minima || password1.length > longitud_maxima) || (password2.length < longitud_minima || password2.length > longitud_maxima)) {
+            if ((password1.length < longitud_minima || password1.length > longitud_maxima) || (password2.length < longitud_minima || password2.length > longitud_maxima)) {
                 console.clear()
                 console.log("La contraseña debe tener una longitud mayor a 0 y menor a 25000");
                 preguntarContraseñas(); // Volver a preguntar
@@ -35,10 +40,30 @@ const preguntarContraseñas = () => {
                 console.log('Las contraseñas deben contener solo letras minúsculas.');
                 preguntarContraseñas(); // Volver a preguntar
             } else {
-                for (let i = 0; i < password1.length; i++) {
-                    nuevaContraseña += password1[i] + password2[i];
+                if(password1.length >= password2.length){
+                    for (let i = 0; i <= password1.length; i++) {
+                        if(i >= password1.length){
+                            nuevaContraseña += password1[i] + password2[i];
+                        }
+                        else{
+                            nuevaContraseña += password1[i]
+                        }
                 }
                 finalizar();
+                }
+                else {
+                    for (let i = 0; i < password2.length; i++) {
+                    nuevaContraseña += password1[i] + password2[i];
+                    if(i >= password2.length){
+                        nuevaContraseña += password1[i] + password2[i];
+                    }
+                    else{
+                        nuevaContraseña += password2[i]
+                    }
+                }
+                finalizar();
+                }
+                
             }
         });
     });
